e0cf54a44d94c419731bb266b9ab36a9158899c9
Palak Sharma <sharma.pala@husky.neu.edu>
2018-11-15 15:26:19 -0500

    changing the link to be sent 1

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ exports.handler = function(event, context) {
       }
   const textBody = 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
         'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-        'http://'+'/reset?email=' + Snsuseremail + '&' + 'token=' + ResetToken + '\n\n'+
+        'http://'+'reset?email=' + Snsuseremail + '&' + 'token=' + ResetToken + '\n\n'+
         'If you did not request this, please ignore this email and your password will remain unchanged.\n';
         
   const params = {
