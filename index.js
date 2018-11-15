00f8c645755a91779bfa04c43b8a6fafe9381e97
Dhanisha Phadate <phadate.d@husky.neu.edu>
2018-11-14 21:38:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('Loading event');
 var aws = require('aws-sdk');
-var ddb = new aws.DynamoDB({params: {TableName: 'snstable'}});
+var ddb = new aws.DynamoDB({params: {TableName: 'csye6225'}});
 
 exports.handler = function(event, context) {
   console.log(event);
@@ -16,7 +16,7 @@ exports.handler = function(event, context) {
   // });
   
   var getparams = {
-  TableName: 'snstable',
+  TableName: 'csye6225',
   Key: {
     'email' : {S: Snsuseremail},
   }
